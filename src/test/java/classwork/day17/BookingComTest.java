82e994c23e7825cbbd2b7a9cbb39b1d7a46bbf99
romankovtun <9696465@gmail.com>
2024-04-14 00:13:39 +0300

    classwork day16: task in progress

--- src/test/java/classwork/day17/BookingComTest.java
+++ src/test/java/classwork/day17/BookingComTest.java
@@ -1,8 +1,6 @@
 package classwork.day17;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -21,27 +19,20 @@ public class BookingComTest {
     private final static String DEPARTUREL_DAY = String.format("//div[@data-testid='searchbox-datepicker-calendar']/div/*[1]/table/tbody/tr/td/span/span[text()='%s']", LocalDate.now().plusDays(10).getDayOfMonth());
     private final static String SIGN_IN_ALERT_CLOSE = "//button[@aria-label='Dismiss sign-in info.']";
     private final static String SEARCH_BUTTON ="//span[text()='Search']";
+    private final static String SIX_SCORE_RAITING_CHECKBOX = "//div[contains(@id, 'filter_group_review_score_:r')]/div[10]";
+    private final static String SORT_BY = "//span[text()='Sort by:']";
+    private final static String SORT_BY_LOW_TO_HIGH = "//span[text()='Property rating (low to high)']";
 
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
         driver.get("https://booking.com");
-        closeSignInAlertIfDisplayed(driver);
+//        closeSignInAlertIfDisplayed(driver);
+        findElementExplicitlyWaitClick(driver, SIGN_IN_ALERT_CLOSE);
 
         driver.findElement(By.xpath(WHERE_ARE_YOU_GOING)).sendKeys("Париж, Иль-де-Франс, Франция");
-
-        try {
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
-            new WebDriverWait(driver, Duration.ofSeconds(5)).until(
-                    ExpectedConditions.visibilityOfElementLocated(By.xpath(PARIS_CITY_SEARCH_OPTION))
-            ).click();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        } catch (TimeoutException e){
-            System.out.println("Something went wrong.");
-        }
-
-//        driver.findElement(By.xpath(PARIS_CITY_SEARCH_OPTION)).click();
+        driver.findElement(By.xpath(PARIS_CITY_SEARCH_OPTION)).click();
 
 //        driver.findElement(By.xpath(DATES_SEARCHBOX)).click();
         driver.findElement(By.xpath(ARRAIVAL_DAY)).click();
@@ -54,13 +45,34 @@ public class BookingComTest {
 
         driver.findElement(By.xpath(SEARCH_BUTTON)).click();
 
+        WebElement sixScoreRaitingCheckbox = driver.findElement(By.xpath(SIX_SCORE_RAITING_CHECKBOX));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", sixScoreRaitingCheckbox);
+        findElementExplicitlyWaitClick(driver, SIX_SCORE_RAITING_CHECKBOX);
+
+        // TODO
+        WebElement sortBy = driver.findElement(By.xpath(SORT_BY));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", sortBy);
+        sortBy.click();
+        driver.findElement(By.xpath(SORT_BY_LOW_TO_HIGH)).click();
     }
 
-    private static void closeSignInAlertIfDisplayed(WebDriver driver){
+//    private static void closeSignInAlertIfDisplayed(WebDriver driver){
+//        try {
+//            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+//            new WebDriverWait(driver, Duration.ofSeconds(5)).until(
+//                    ExpectedConditions.visibilityOfElementLocated(By.xpath(SIGN_IN_ALERT_CLOSE))
+//            ).click();
+//            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//        } catch (TimeoutException e){
+//            System.out.println("Something went wrong.");
+//        }
+//    }
+
+    private static void findElementExplicitlyWaitClick(WebDriver driver, String by){
         try {
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
             new WebDriverWait(driver, Duration.ofSeconds(5)).until(
-                    ExpectedConditions.visibilityOfElementLocated(By.xpath(SIGN_IN_ALERT_CLOSE))
+                    ExpectedConditions.visibilityOfElementLocated(By.xpath(by))
             ).click();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         } catch (TimeoutException e){
